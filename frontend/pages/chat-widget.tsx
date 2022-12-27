bd1bdfebbefba1c427906fb411bfb78edd80568d
Ifeanyi Dike <ifeanyidike87@gmail.com>
2022-12-27 23:36:59 +0100

    design the widget chatts interface

--- frontend/pages/chat-widget.tsx
+++ frontend/pages/chat-widget.tsx
@@ -4,12 +4,15 @@ import useTypingIndicator from '../hooks/useTypingIndicator';
 // import { ChatWidgetContainer } from '../styles/ChatWidgetStyles';
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
-import loadingProgress from '../assets/gifs/loading_progress.gif';
 
 import { Inter } from '@next/font/google';
 import EmojiIcon from '../assets/components/EmojiIcon';
 import SendIcon from '../assets/components/SendIcon';
 import Image from 'next/image';
+import ChatButton from '../assets/components/ChatButton';
+import CloseIcon from '../assets/components/CloseIcon';
+import TypingAnimation from '../assets/gifs/typing-animation-3x.gif';
+
 const inter = Inter({ subsets: ['latin'] });
 
 const ChatWidget: FC<ReactNode> = () => {
@@ -26,8 +29,18 @@ const ChatWidget: FC<ReactNode> = () => {
   const { socket } = useSocket();
   const [userTyping, setUserTyping] = useState('');
   const [widgetOpen, toggleWidget] = useState<undefined | boolean>(undefined);
+  const [isInIframe, setIsInIframe] = useState(false);
 
   const isTyping = useTypingIndicator(inputRef);
+
+  // useEffect(() => {
+  //   const isInIframe = window.location !== window.parent.location;
+  //   if (!isInIframe) {
+  //     toggleWidget(true);
+  //   } else {
+  //     toggleWidget(false);
+  //   }
+  // }, []);
 
   const bindEvent = (
     element: Window | Document,
@@ -40,6 +53,10 @@ const ChatWidget: FC<ReactNode> = () => {
   };
 
   useEffect(() => {
+    setIsInIframe(window.location !== window.parent.location);
+  }, []);
+
+  useEffect(() => {
     if (isTyping === undefined || !socket) return;
     socket.emit('isTyping', { isTyping, user });
 
@@ -47,9 +64,11 @@ const ChatWidget: FC<ReactNode> = () => {
   }, [isTyping, socket]);
 
   useEffect(() => {
+    console.log({ socket });
     if (!socket) return;
     socket.on('message', (data: any) => console.log({ data }));
     socket.on('isTyping', (data: any) => {
+      console.log('IN SOCKET', data);
       if (data.isTyping) {
         setUserTyping(`${data.user.name} is typing`);
       } else {
@@ -60,10 +79,10 @@ const ChatWidget: FC<ReactNode> = () => {
 
   useEffect(() => {
     bindEvent(window, 'message', function (e) {
-      console.log('message from widget', e);
+      // console.log('message from widget', e);
     });
     window.addEventListener('message', e => {
-      console.log('message', e);
+      // console.log('message', e);
     });
   }, [widgetOpen]);
 
@@ -85,42 +104,79 @@ const ChatWidget: FC<ReactNode> = () => {
     }
   };
 
+  const openWidget = () => {
+    if (!isInIframe) return true;
+
+    return widgetOpen;
+  };
+  // console.log({ userTyping });
   return (
     <>
-      {!widgetOpen ? (
-        <button onClick={() => toggleWidget(!widgetOpen)}>Trigger</button>
+      {!openWidget() ? (
+        <button className="chaticon" onClick={() => toggleWidget(!widgetOpen)}>
+          <ChatButton />
+          {/* <Image
+            src={ChatImage Icon}
+            alt="Chat button"
+            style={{
+              // color: 'red',
+              width: 150,
+              height: 150,
+              backgroundColor: 'transparent',
+            }}
+          /> */}
+        </button>
       ) : (
         <form
           className={`chatwidget ${inter.className}`}
           ref={formRef}
           onSubmit={handleSubmit}
         >
-          <button onClick={() => toggleWidget(!widgetOpen)}>Close</button>
-          <div className="chatwidget__messagelist">
-            {userTyping && (
-              <span className="messagelist__typingindicator">{userTyping}</span>
+          <div className="chatwidget__top">
+            <div className="chatwidget__topinfo">
+              <div className="onlineindicator"></div>
+              <span>Chat with us now, we are online</span>
+            </div>
+            {isInIframe && (
+              <button
+                className="close"
+                onClick={() => toggleWidget(!widgetOpen)}
+              >
+                <CloseIcon />
+              </button>
             )}
           </div>
-          {openEmoji && (
-            <div className="chatwidget__emojipicker">
-              <Picker data={data} onEmojiSelect={console.log} />
-            </div>
-          )}
-          <EmojiIcon
-            className="chatwidget__emojiopener"
-            openEmoji={openEmoji}
-            setOpenEmoji={setOpenEmoji}
-          />
-          <textarea
-            className="chatwidget__input"
-            ref={inputRef}
-            onChange={e => setMessage(e.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-          <button aria-label="Send" className="chatwidget__send">
-            <SendIcon />
-          </button>
-          {/* <button onClick={handleClick}>Send Socket</button> */}
+          <div className="chatwidget__messagelist">
+            {userTyping && (
+              <span className="messagelist__typingindicator">
+                {userTyping} Please wait
+              </span>
+            )}
+          </div>
+          <div className="chatwidget__messageinput">
+            {openEmoji && (
+              <div className="chatwidget__emojipicker">
+                <Picker data={data} onEmojiSelect={console.log} />
+              </div>
+            )}
+            <EmojiIcon
+              className="chatwidget__emojiopener"
+              openEmoji={openEmoji}
+              setOpenEmoji={setOpenEmoji}
+            />
+            <textarea
+              className="chatwidget__input"
+              ref={inputRef}
+              onChange={e => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            {!isInIframe && (
+              <button aria-label="Send" className="chatwidget__send">
+                <SendIcon />
+              </button>
+            )}
+            {/* <button onClick={handleClick}>Send Socket</button> */}
+          </div>
         </form>
       )}
     </>
